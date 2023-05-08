4b061d6f1acc931021da7dcbe64cca1e1638e922
biyas606 <biyas606@yahoo.in>
2023-05-09 01:19:20 +0530

    login functionality

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -1,4 +1,32 @@
 package controllers;
 
+import models.LoginModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import repositories.PharmacyRepository;
+
+import java.util.HashMap;
+
+@RestController
 public class LoginController {
+
+    @Autowired
+    PharmacyRepository pharmacyRepository;
+
+    @PostMapping(path = "login", consumes = "application/json", produces = "application/json")
+    @ResponseBody
+    public HashMap login(@RequestBody LoginModel loginModel) {
+        HashMap<String, Object> hashMap = new HashMap<>();
+        int value=pharmacyRepository.login(loginModel.getUserName(), loginModel.getPassword());
+        //System.out.println("value"+value);
+        if(value>=1)
+            hashMap.put("status","Login success");
+        else
+            hashMap.put("status","Login failed");
+        return hashMap;
+    }
+
 }
